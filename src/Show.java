7c2168c641fea9219395b57c24799c8934a113ef
IgorNikolaev <grnklv@yandex.ru>
2025-10-24 16:53:17 +0300

    Классы готовы

--- src/Show.java
+++ src/Show.java
@@ -6,9 +6,15 @@ public class Show {
     Director director;
     ArrayList<Actor> actors;
 
-    public Show(String title, double duration, Director director) {
+
+
+    public Show(String title, double duration, Director director, ArrayList<Actor> actors) {
         this.title = title;
         this.duration = duration;
         this.director = director;
+        this.actors = actors;
+    }
+
+    public Show() {
     }
 }
